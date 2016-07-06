fe54a9cd1567a34644497bf650c1115065c26898
lee1903 <lee1903@purdue.edu>
2016-07-05 21:00:30 -0700

    added mongoose uri for heroku

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ var bodyParser = require('body-parser');
 var routes     = require('./routes');
 
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/restdb')
+// mongoose.connect('mongodb://localhost/restdb')
+
+var uristring = 'mongodb://heroku_txjq8jr7:1hsdjvrs0u9pi35q4i9q2k3nt4@ds015335.mlab.com:15335/heroku_txjq8jr7'
+
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+      console.log ('Succeeded connected to: ' + uristring);
+    }
+});
  
 // express app will use body-parser to get data from POST
 app.use(bodyParser.urlencoded({ extended: true }));
